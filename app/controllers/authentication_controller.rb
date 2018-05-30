56251a63f334c4445bb776274a43c9cf2d987a42
Naoki Ishimura <mirontest1460@gmail.com>
2018-05-30 20:56:49 +0900

    Add token authentication

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,20 @@
+class AuthenticationController < ApplicationController
+  # CSRF対策を無効にする
+  protect_from_forgery except: :authenticate
+  
+  def authenticate
+    user = User.find_by(token: authentication_params[:token])
+    if user
+      session[:user_id] = user.id
+      render json: { 'message': 'Authentication success' }, status: :ok
+    else
+      render json: { 'message': 'Authentication failure' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def authentication_params
+    params.require(:authentication).permit(:token,)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   resource :sessions, only: [:new, :create, :destroy]
   match '/login', to: 'sessions#new', via: 'get'
   match '/logout', to: 'sessions#destroy', via: 'delete'
+  
+  ##
+  # authentication
+  post '/authentication', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
