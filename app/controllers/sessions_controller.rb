2895f1e74ab80a769f1b9bda0c9325eb605147eb
Naoki Ishimura <mirontest1460@gmail.com>
2018-05-30 20:29:22 +0900

    Refactoring

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def create
     # メールアドレスでユーザーを検索
-    user = User.find_by(name: params[:session][:name])
+    user = User.find_by(name: session_params[:name])
     # パスワードの一致を検証
-    if user && user.authenticate(params[:session][:name])
+    if user && user.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to user, notice: 'ログイン成功'
     else
@@ -14,5 +14,11 @@ class SessionsController < ApplicationController
   def destroy
     reset_session
     redirect_to root_path
+  end
+  
+  private
+  
+  def session_params
+    params.require(:session).permit(:name, :password)
   end
 end
