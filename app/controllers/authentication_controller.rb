36d4254017cd3736d6767748681784884a9794a8
Naoki Ishimura <mirontest1460@gmail.com>
2018-06-01 22:32:08 +0900

    Enable CSRF protect

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,6 @@
 class AuthenticationController < ApplicationController
   # CSRF対策を無効にする
-  protect_from_forgery except: :authenticate
+  # protect_from_forgery except: :authenticate
   
   def authenticate
     user = User.find_by(token: authentication_params[:token])
